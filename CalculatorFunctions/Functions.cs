eb94926c0d4ded1526dc17e1c5c3aba87a7151b6
Peteris <peterisecis@gmail.com>
2022-05-03 10:29:30 +0300

    Add project files.

---
+++ CalculatorFunctions/Functions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorFunctions
+{
+    public static class Functions
+    {
+        public static double Add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+    }
+}
