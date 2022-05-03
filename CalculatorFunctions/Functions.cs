c6da91a3c4958006f048ef0b9983dfd83c2c52d2
Peteris <peterisecis@gmail.com>
2022-05-03 11:01:19 +0300

    Now returns null if dividing by 0

--- CalculatorFunctions/Functions.cs
+++ CalculatorFunctions/Functions.cs
@@ -2,23 +2,29 @@
 {
     public static class Functions
     {
-        public static double Add(double num1, double num2)
+        public static double? Add(double num1, double num2)
         {
             return num1 + num2;
         }
-        public static double Subtract(double num1, double num2)
+        public static double? Subtract(double num1, double num2)
         {
             return num1 - num2;
         }
-        public static double Multiply(double num1, double num2)
+        public static double? Multiply(double num1, double num2)
         {
             return num1 * num2;
         }
-        public static double Divide(double num1, double num2)
+        public static double? Divide(double num1, double num2)
         {
-            //For now this function is left without any checks intentionally
-            //Later I will add safety checks if num2 is not 0
-            return num1 / num2;
+            //Now this function will return null if num2 is 0 and throws error
+            try
+            {
+                return num1 / num2;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
