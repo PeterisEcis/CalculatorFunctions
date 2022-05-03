bb07b4f451b527bc7984a7bd13a8ff0e0a3b72be
Peteris <peterisecis@gmail.com>
2022-05-03 10:57:26 +0300

    All functions return null

--- CalculatorFunctions/Functions.cs
+++ CalculatorFunctions/Functions.cs
@@ -2,23 +2,22 @@
 {
     public static class Functions
     {
-        public static double Add(double num1, double num2)
+        //All functions return null
+        public static double? Add(double num1, double num2)
         {
-            return num1 + num2;
+            return null;
         }
-        public static double Subtract(double num1, double num2)
+        public static double? Subtract(double num1, double num2)
         {
-            return num1 - num2;
+            return null;
         }
-        public static double Multiply(double num1, double num2)
+        public static double? Multiply(double num1, double num2)
         {
-            return num1 * num2;
+            return null;
         }
-        public static double Divide(double num1, double num2)
+        public static double? Divide(double num1, double num2)
         {
-            //For now this function is left without any checks intentionally
-            //Later I will add safety checks if num2 is not 0
-            return num1 / num2;
+            return null;
         }
     }
 }
