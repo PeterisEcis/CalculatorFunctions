e3172160ccc4d6bfa67edda1f6af1e0476eb3462
Peteris <peterisecis@gmail.com>
2022-05-03 10:44:01 +0300

    Compiled first dll file and I removed Debug folder from gitignore so that I could save different versions of the dll file

--- CalculatorFunctions/Functions.cs
+++ CalculatorFunctions/Functions.cs
@@ -1,16 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CalculatorFunctions
+﻿namespace CalculatorFunctions
 {
     public static class Functions
     {
-        public static double Add(double num1, double num2)
+        static double Add(double num1, double num2)
         {
             return num1 + num2;
+        }
+        static double Subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        static double Multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+        static double Divide(double num1, double num2)
+        {
+            //For now this function is left without any checks intentionally
+            //Later I will add safety checks if num2 is not 0
+            return num1 / num2;
         }
     }
 }
