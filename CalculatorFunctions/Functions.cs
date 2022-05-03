5569aa88cff6881b0014911ce8e5e015d5ae0df1
Peteris <peterisecis@gmail.com>
2022-05-03 10:52:30 +0300

    Had to make functions public

--- CalculatorFunctions/Functions.cs
+++ CalculatorFunctions/Functions.cs
@@ -2,19 +2,19 @@
 {
     public static class Functions
     {
-        static double Add(double num1, double num2)
+        public static double Add(double num1, double num2)
         {
             return num1 + num2;
         }
-        static double Subtract(double num1, double num2)
+        public static double Subtract(double num1, double num2)
         {
             return num1 - num2;
         }
-        static double Multiply(double num1, double num2)
+        public static double Multiply(double num1, double num2)
         {
             return num1 * num2;
         }
-        static double Divide(double num1, double num2)
+        public static double Divide(double num1, double num2)
         {
             //For now this function is left without any checks intentionally
             //Later I will add safety checks if num2 is not 0
